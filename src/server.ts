05f67925f8b3f5017ffc572ad3433b5e5c86bf8d
New-AH <thomas.george@edu.ece.fr>
2019-11-24 12:14:23 +0100

    Finish

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,8 @@ const app = express()
 const port: string = process.env.PORT || '8080'
 
 app.get('/', (req: any, res: any) => {
-  res.write('Hello world')
+  res.write('Hello, your are in the home page. \n')
+  res.write('In order to continue, you should add to the url "/hello/[your name]"')
   res.end()
 })
 
@@ -16,6 +17,22 @@ app.get('/metrics.json', (req: any, res: any) => {
       }
       res.json(result)
     })
+  })
+
+app.get('/:name', (req: any, res: any) => {
+    res.write('Error 404. \n')
+    res.write('Page not found')
+    res.end()
+  })
+app.get('/hello/thomas', (req: any, res: any) => {
+    res.write('Hello, my name is Thomas. \n')
+    res.write('I am 22 years old and I am studing engineering at ECE Paris.')
+    res.end()
+  })
+
+app.get('/hello/:name', (req: any, res: any) => {
+    res.write('Hello Anonymous ! \n')
+    res.end()
   })
 
 app.listen(port, (err: Error) => {
